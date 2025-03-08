14995af51ff78d888bae774c1c2b3ab5dff7c3c5
suched2513 <120272081+suched2513@users.noreply.github.com>
2025-03-08 13:14:55 +0000

    แก้ไข2

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export default {
-    aprUrl: "https://script.google.com/macros/s/AKfycbxVUinvWzsp178oiD7ldBJFlc3J-Bke1377PsJ9uuBIbrKgI-TlxuO0xtzmqSV3af0b/exec"
+    aprUrl: "https://script.google.com/macros/s/AKfycbwR7Cfx5SbaES_-gb2Q5irhqwmEyqvBIUxOVKf3E-IvolrIks6X_krKys6dO-4hdr0n/exec"
 }
